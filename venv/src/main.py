e77a97b9a1cc8a49790eaa915123f4adebad51e9
Miguel Flórez <miguelflorez@MacBook-Air-de-Miguel.local>
2021-04-15 23:00:14 +0200

    Movimiento de cabeza terminado

--- venv/src/main.py
+++ venv/src/main.py
@@ -6,9 +6,19 @@ size = 500, 500
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('SNAKE')
 
+FPS = 10
+fps_clock = pygame.time.Clock()
+
 GREEN = (45, 87, 44)
 GREY = (155, 155, 155)
 BLUE = (0, 0, 255)
+
+x_snake = 400
+y_snake = 200
+x_direction = 0
+y_direction = 0
+x_speed = 0
+y_speed = 0
 
 class Snake():
     def __init__(self):
@@ -20,7 +30,7 @@ class Snake():
 def update_screen():
     screen.fill(GREEN)
     draw_grid(500, 500, 20)
-    snake.draw_snake(400, 200, 20, 20)
+    snake.draw_snake(x_snake, y_snake, 20, 20)
     pygame.display.update()
 
 def draw_grid(height, width, rect_size):
@@ -46,21 +56,34 @@ while run:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                x_movement = 0
-                y_movement = -10
+                x_speed = 0
+                y_speed = -20
 
             if event.key == pygame.K_DOWN:
-                x_movement = 0
-                y_movement = 10
+                x_speed = 0
+                y_speed = 20
 
             if event.key == pygame.K_RIGHT:
-                x_movement = 10
-                y_movement = 0
+                x_speed = 20
+                y_speed = 0
 
             if event.key == pygame.K_LEFT:
-                x_movement = -10
-                y_movement = 0
+                x_speed = -20
+                y_speed = 0
+
+    x_direction = x_snake + x_speed
+    y_direction = y_snake + y_speed
+
+    x_snake = x_direction
+    y_snake = y_direction
+
+    if x_snake >= 500:
+        run = False
+
+    if y_snake >= 500:
+        run = False
 
     update_screen()
+    fps_clock.tick(FPS)
 
 pygame.quit()
